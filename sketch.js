9bad48ef38d724a1294805e3efe7606e670e622d
Sweytha <73808022+Sweytha@users.noreply.github.com>
2021-10-29 10:14:01 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
+                            "ship-2.png","ship-1.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  
+}
+
+function draw() {
+  background(0);
+  
+    
+  drawSprites();
+}
